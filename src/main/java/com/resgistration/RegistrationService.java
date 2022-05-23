67aedbb4f75ab82d29484d3ea6741dcea245d023
jahnavimachireddy <93317547+JAHNAVI-MACHIREDDY-WTT@users.noreply.github.com>
2022-05-23 12:58:56 +0530

    changes

--- src/main/java/com/resgistration/RegistrationService.java
+++ src/main/java/com/resgistration/RegistrationService.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegistrationService {
     String register(RegistrationRequest request) {
+<<<<<<< Updated upstream
         return "it works";
+=======
+
+        return null;
+>>>>>>> Stashed changes
     }
 }
--- src/main/java/com/users/UserService.java
+++ src/main/java/com/users/UserService.java
@@ -8,8 +8,14 @@ import org.springframework.stereotype.Service;
 
     @Service
     @AllArgsConstructor
+<<<<<<< Updated upstream
     public class UserService implements UserDetailsService {
         private UserRepository usersRepository;
+=======
+    public class UsersService implements UserDetailsService {
+        private final static String  USER_NOT_FOUND_NSG="User not found";
+        private final  UserRepository usersRepository;
+>>>>>>> Stashed changes
         @Override
         public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
             return (UserDetails) usersRepository.findByEmail(email).orElseThrow(()->new UsernameNotFoundException("user name not found"));
